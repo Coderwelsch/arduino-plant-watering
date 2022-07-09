d7068a236f79bac691b05799f9c564cc098eb798
Joseph Ribbe <social@coderwelsch.com>
2022-07-09 22:58:28 +0200

    Improved code quality

--- ArduinoPlantWatering/ArduinoPlantWatering.ino
+++ ArduinoPlantWatering/ArduinoPlantWatering.ino
@@ -1,10 +1,5 @@
 #define MIN_MOISTURE_PERCENTAGE 80
 
-// despite the length of your pump water tubes you should
-// test the optimum delay the water needs to travel to your planty
-#define PUMP_TIME_OFFSET 2000
-#define PUMP_DURATION 2000
-
 #define MIN_MOISTURE_VAL 346
 #define MAX_MOISTURE_VAL 540
 
@@ -17,12 +12,17 @@ struct ConfigStruct {
     int moistureSensorPin;
     int relayInputPin;
     int moistureValue;
+    // despite the length of your pump water tubes you should
+    // test the optimum delay the water needs to travel to your plantie <3
+    int pumpTimeOffset;
+    // the main duration of pumping water
+    int pumpDuration;
 };
 
 // your flower config
-struct ConfigStruct FLOWERING_SETS[SETS_COUNT] = { 
-    { "Tomato #1", A0, 13, 0 } /*, 
-    { "Tomato #2", A1, 12, 0 } */
+struct ConfigStruct FLOWER_SET[SETS_COUNT] = { 
+    { "Tomato #1", A0, 13, 0, 2000, 4000 } /*,
+    { "Tomato #2", A1, 12, 0, 2000, 2000 } */
 };
 
 
@@ -30,7 +30,7 @@ void setup() {
     Serial.begin(9600);
   
     for (int i = 0; i < SETS_COUNT; ++i ) {
-        int pumpPin = FLOWERING_SETS[i].relayInputPin;
+        int pumpPin = FLOWER_SET[i].relayInputPin;
         pinMode(pumpPin, OUTPUT);
     }
 }
@@ -50,20 +50,23 @@ void watering () {
 }
 
 void wateringSet (int setIndex) {
-    int pumpPin = FLOWERING_SETS[setIndex].relayInputPin;
-    int moistureValue = FLOWERING_SETS[setIndex].moistureValue;
-    String setName = String(FLOWERING_SETS[setIndex].displayName);
+    String setName = String(FLOWER_SET[setIndex].displayName);
+
+    int pumpPin = FLOWER_SET[setIndex].relayInputPin;
+    int moistureValue = FLOWER_SET[setIndex].moistureValue;
+    int pumpTimeOffset = FLOWER_SET[setIndex].pumpTimeOffset;
+    int pumpDuration = FLOWER_SET[setIndex].pumpDuration;
 
     // when measured moisture value felt below the
     // "dry" zone -> pump some water
     if (moistureValue < MIN_MOISTURE_PERCENTAGE) {
-        float pumpValueOffset = float(MIN_MOISTURE_PERCENTAGE) / float(moistureValue);
-        int computedPumpTime = PUMP_TIME_OFFSET + PUMP_DURATION * pumpValueOffset;
+        float pumpValueOffset = (float(MIN_MOISTURE_PERCENTAGE) / 100) * (float(100 - moistureValue) / 100) * 2000;
+        int computedPumpTime = pumpTimeOffset + pumpDuration + pumpValueOffset;
 
         float timeInSecs = computedPumpTime / 1000;
         
         Serial.println(
-            "Watering set \"" + setName + "\"" + 
+            "Watering set «" + setName + "»" +
             ", Moisture: " + String(moistureValue) + " %" +
             ", Duration: " + String(timeInSecs) + " s"
         );
@@ -78,18 +81,18 @@ void wateringSet (int setIndex) {
 
 void setPumpState (int pin, bool active) {
     if (active) {
-        digitalWrite(pin, HIGH);
+        //digitalWrite(pin, HIGH);
     } else {
-        digitalWrite(pin, LOW);
+        //digitalWrite(pin, LOW);
     }
 }
 
 void readValues () {
     for (int i = 0; i < SETS_COUNT; ++i ) {
-        int moisturePin = FLOWERING_SETS[i].moistureSensorPin;
+        int moisturePin = FLOWER_SET[i].moistureSensorPin;
         
         // saves moisture value
-        FLOWERING_SETS[i].moistureValue = getMoisturePercentage(moisturePin);
+        FLOWER_SET[i].moistureValue = getMoisturePercentage(moisturePin);
     }
 }
 
@@ -98,8 +101,6 @@ int getMoisturePercentage(int pin) {
     
     // give the arduino some time to read the analogue value carefully
     delay(1);
-
-    // Serial.println(String(pin) + ": " + String(value));
 
     // reset value to bounces
     if (value < MIN_MOISTURE_VAL) {
